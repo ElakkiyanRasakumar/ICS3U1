1f0b59f266c3d44d569dde9280c6ea62c4bd03d1
Elakkiyan <elakkiyan2006@gmail.com>
2023-03-06 10:54:23 -0500

    Finished List 1

---
+++ List1/make_ends.py
@@ -0,0 +1,5 @@
+def make_ends(nums):
+    c= []
+    c.append(nums[0])
+    c.append(nums[-1])
+    return c
---
+++ List1/middle_way.py
@@ -0,0 +1,5 @@
+def middle_way(a, b):
+    c = []
+    c.append(a[1])
+    c.append(b[1])
+    return c
