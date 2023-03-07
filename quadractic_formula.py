4994f4db70ba2dca3c8320895e410be1c0298e12
Elakkiyan <elakkiyan2006@gmail.com>
2023-03-07 11:16:30 -0500

    Updated for Sheet2

--- quadractic_formula.py
+++ quadractic_formula.py
@@ -10,13 +10,15 @@ def main():
     a_value = int(input("What is your a value? "))
     b_value = int(input("What is your b value? "))
     c_value = int(input("What is your c value? "))
-    x1, x2= quadratic_formula(a_value, b_value, c_value)
-    # print(quadratic_formula(a_value, b_value, c_value))
-    print(x1, x2)
+    quadratic_formula(a_value, b_value, c_value)
+    # print(x1, x2)
 def quadratic_formula(a, b, c):
-    x1 = (-(b) + sqrt((b**2)-(4*a*c)))/(2*a)
-    x2 = (-(b) - sqrt((b**2)-(4*a*c)))/(2*a)
-    return x1, x2
+    d = (b**2) - (4*a*c)
+    if d < 0:
+        return print("There are no specific roots")
+    x1 = (-(b) + sqrt(d))/(2*a)
+    x2 = (-(b) - sqrt(d))/(2*a)
+    return print(x1, x2)
 
 
 main()
